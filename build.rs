803c75893df3ae9d0f87317479a11490c6c0b855
lstarch <138353694+lstarch@users.noreply.github.com>
2022-02-19 19:50:32 +0000

    Fixing some bugs.

--- build.rs
+++ build.rs
@@ -10,4 +10,4 @@ use std::io::Write;
 fn main() {
     let cur_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let mut status = File::create("/tmp/car
+    let mut status = File::create("/tmp/cargo.
