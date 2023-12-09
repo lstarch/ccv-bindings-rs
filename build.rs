a32cedead0f4b552348758f54e5e32d6e4b27d3a
lstarch <138353694+lstarch@users.noreply.github.com>
2023-12-09 09:28:06 +0000

    Update KBaseFBAModeling.md

--- build.rs
+++ build.rs
@@ -39,4 +39,4 @@ fn main() {
     println!("cargo:rustc-link-lib=ccv");
 
     // Now handle libpng
-    let lib = pkg_config::Config::new().statik(false).atleast_version("1.6").probe("libpng").expect("Could
+    let lib = pkg_config::Config::new().statik(false).atleast_version("1.6").probe("libpng").expect("Could n
