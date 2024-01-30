e1b54ab45d2edb38bb2387044c9cbbea9b101ade
lstarch <138353694+lstarch@users.noreply.github.com>
2024-01-30 06:15:57 +0000

    Trailing spaces.

--- build.rs
+++ build.rs
@@ -44,4 +44,5 @@ fn main() {
     // Link path
     for path in &lib.link_paths {
         println!("cargo:rustc-link-search={}", path.to_str().expect("Could not convert path to str"));
-   
+    }
+
