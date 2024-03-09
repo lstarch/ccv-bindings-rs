4b950c44d07a15b8a225a4b8345d3d4ea9e3cc98
lstarch <138353694+lstarch@users.noreply.github.com>
2024-03-09 07:11:29 +0000

    removed redundant tests

--- examples/swtdetect.rs
+++ examples/swtdetect.rs
@@ -14,3 +14,7 @@ fn main() {
     let file_in = args.next().expect("Expected an input file");
     let file_out = args.next().expect("Expected an output file");
     let mut pix = Matrix::read(file_in, OpenAs::ToGray).expect("Could not read image");
+
+    let start = Instant::now();
+    let words = pix.detect_words(Default::default());
+    let duration = Instant::now() - start;
